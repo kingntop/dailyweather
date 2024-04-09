8f0b56847a5d3db8efab3f5976a662f082f77f89
limyoungmin <kingntop@naver.com>
2024-04-09 09:48:38 +0900

    6

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 window.onload = function() {
     // OpenWeatherMap API 키
-    var apiKey = '1660a1fb864ce7d9d29bb47b6fa0efeb';
+    var apiKey = 'f302c3976c10428a0e5467cce4b54984';
     // API 호출 URL
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=' + apiKey + '&units=metric';
-    
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=' + apiKey;
+
     // API 호출
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        // 데이터 받아오기
-        var weatherData = {
-            city: data.name,
-            temperature: data.main.temp,
-            description: data.weather[0].description,
-            icon: data.weather[0].icon
-        };
+        // 예보 데이터 받아오기
+        var forecastData = data.list.map(item => ({
+            time: item.dt_txt,
+            temperature: item.main.temp,
+            description: item.weather[0].description,
+            icon: item.weather[0].icon
+        }));
 
-        // 날씨 정보 HTML에 추가
-        var weatherElement = document.getElementById('weather');
-        weatherElement.innerHTML = `
-            <h2>${weatherData.city}</h2>
-            <p>온도: ${weatherData.temperature}℃</p>
-            <p>날씨: ${weatherData.description}</p>
-            <img src="http://openweathermap.org/img/wn/${weatherData.icon}.png" alt="weather-icon">
-        `;
+        // 날씨 예보 정보 HTML에 추가
+        var forecastElement = document.getElementById('forecast');
+        forecastData.forEach(forecast => {
+            forecastElement.innerHTML += `
+                <div class="forecast-item">
+                    <p>${forecast.time}</p>
+                    <p>온도: ${forecast.temperature}℃</p>
+                    <p>날씨: ${forecast.description}</p>
+                    <img src="http://openweathermap.org/img/wn/${forecast.icon}.png" alt="weather-icon">
+                </div>
+            `;
+        });
     })
-    .catch(error => console.log('Error fetching weather data:', error));
+    .catch(error => console.log('Error fetching weather forecast data:', error));
 };
